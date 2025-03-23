e3820e10ac98f7f79de4c32a4677059492503fe6
raghull15 <rbraghul1508@gmail.com>
2025-03-24 00:54:52 +0530

    23

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(){
+  return(
+      <div >
+        <h1>This is your react app</h1>
+        <a href="static.html" >Click here to vist html page</a>
+      </div>
+  );
+}
+
+export default App;
